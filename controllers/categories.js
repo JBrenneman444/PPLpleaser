2621cc541e4bdb6a449767c04b874204605c841d
LocalPlacesWith <jbrenneman444@gmail.com>
2020-03-08 21:49:00 -0700

    working on CONTESTANT and goin to sleep

--- controllers/categories.js
+++ controllers/categories.js
@@ -12,18 +12,6 @@ router.get('/new', (req, res)=>{
     });
 });
 
-// NEW-CONTESTANT
-router.get('/:id/new-contestant', (req, res)=>{
-  Category.findById(req.params.id, (err, foundCategory)=>{ //find the log
-      res.render('categories/new-contestant.ejs',
-      {
-        category: foundCategory,
-        currentUser: req.session.currentUser
-      }
-    );
-  });
-});
-
 // CREATE / POST
 router.post('/',(req,res)=>{
     if(req.body.img == ''){
@@ -49,6 +37,19 @@ router.get('/:id/edit', (req, res)=>{
     });
   });
 
+  
+// NEW-CONTESTANT
+router.get('/:id/new-contestant', (req, res)=>{
+  Category.findById(req.params.id, (err, foundCategory)=>{ //find the log
+      res.render('categories/new-contestant.ejs',
+      {
+        category: foundCategory,
+        currentUser: req.session.currentUser
+      }
+    );
+  });
+});
+
 // PUT Route for BUY BUTTON -- N O T  D O N E  Y E T  !!
 router.put('/:id/voted', (req, res) => {
   Category.findByIdAndUpdate(
@@ -60,6 +61,18 @@ router.put('/:id/voted', (req, res) => {
   });
 });
 
+
+// UPDATE / PUT Route
+router.put('/:id/contestant', (req, res)=>{
+  if(req.body.contestants.img == ''){
+      req.body.contestants.img = "https://image.shutterstock.com/image-vector/no-image-available-sign-absence-260nw-373243873.jpg";
+  } else {
+  // do nothing
+  }
+      Category.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel)=>{
+        res.redirect(`/categories/${req.params.id}`)
+      });
+  });
 
 // UPDATE / PUT Route
 router.put('/:id', (req, res)=>{
--- models/categories.js
+++ models/categories.js
@@ -1,15 +1,26 @@
 const mongoose = require('mongoose');
 
+const contestantSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        img: String,
+        votes: Number
+    },
+    { timestamps: true }
+)
+
 const categorySchema = new mongoose.Schema(
     {
         name: {type: String, required: true },
         img: String,
-        contestants: Array,
+        contestants: [contestantSchema],
         votes: Number
     },
     { timestamps: true }
 );
 
 const Category = mongoose.model('Category', categorySchema);
+const Contestant = mongoose.model('Contestant', categorySchema);
 
 module.exports = Category;
+module.exports = Contestant;
